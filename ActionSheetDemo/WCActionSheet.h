629492eaa05333ddc0c50181ebdb47818a3cda2a
Zach <zach_41@163.com>
2016-01-28 11:23:45 +0800

    initial

---
+++ ActionSheetDemo/WCActionSheet.h
@@ -0,0 +1,13 @@
+//
+//  WCActionSheet.h
+//  ActionSheetDemo
+//
+//  Created by ZachZhang on 16/1/27.
+//  Copyright © 2016年 ZachZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCActionSheet : UIView
+
+@end
---
+++ ActionSheetDemo/WCActionSheet.m
@@ -0,0 +1,21 @@
+//
+//  WCActionSheet.m
+//  ActionSheetDemo
+//
+//  Created by ZachZhang on 16/1/27.
+//  Copyright © 2016年 ZachZhang. All rights reserved.
+//
+
+#import "WCActionSheet.h"
+
+@implementation WCActionSheet
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
